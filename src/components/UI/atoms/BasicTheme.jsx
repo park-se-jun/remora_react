928520bc287103db05ee84cedb290c3689e23c4f
Sejun Park <qkrtpwns1999@naver.com>
2021-11-22 02:49:26 +0900

    feat: Theme 추가 meterial 디자인에 사용하는 theme 추가

---
+++ src/components/UI/atoms/BasicTheme.jsx
@@ -0,0 +1,17 @@
+import { createTheme } from "@material-ui/core";
+
+const BasicTheme = createTheme({
+    palette: {
+        primary: {
+            dark: "#4d759a",
+            ight: "#8bb9e3",
+            main: "#6fa8dc",
+        },
+        secondary: {
+            light: "#eaeaea",
+            dark: "#a0a0a0",
+            main: "#e5e5e5",
+        },
+    },
+});
+export default BasicTheme;

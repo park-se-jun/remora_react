5e5bf874107053285d1b585d9305f6eb2bf31ad2
Sejun Park <qkrtpwns1999@naver.com>
2021-11-29 02:42:42 +0900

    style : mui 버전 업, 색 변경 mui 라이브러리 버전업, 버튼 색깔 변경

--- src/components/UI/atoms/BasicButton.jsx
+++ src/components/UI/atoms/BasicButton.jsx
@@ -1,13 +1,13 @@
-import { Button } from "@material-ui/core";
+import { Button } from "@mui/material";
 import PropTypes from "prop-types";
-import { ThemeProvider } from "@material-ui/styles";
+import { ThemeProvider } from "@mui/material/styles";
 
 import BasicTheme from "./BasicTheme";
 
 function BasicButton({ text }) {
     return (
         <ThemeProvider theme={BasicTheme}>
-            <Button color="primary" variant="contained">
+            <Button color="secondary" variant="contained">
                 {text}
             </Button>
         </ThemeProvider>

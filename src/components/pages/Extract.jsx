66ce110061567c55c7f6a72a31b8d6e227aad6b3
Sejun Park <qkrtpwns1999@naver.com>
2021-12-06 14:44:20 +0900

    feat : 탭뷰로 라우팅 추가 탭뷰에 라우팅 기능 추가

--- src/components/pages/Extract.jsx
+++ src/components/pages/Extract.jsx
@@ -1,7 +1,10 @@
+import RemoraHeader from "components/UI/atoms/RemoraHeader";
+
 function Extract() {
     return (
         <div className="Extreact" style={{ padding: "1rem 0" }}>
-            <h2>extract 페이지 입니다.</h2>
+            <h2>extract 페이지</h2>
+            <RemoraHeader index={2} />
         </div>
     );
 }

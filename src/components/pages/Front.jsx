62d779d752cb11ab0c0485e4e5c816a76748e51d
Sejun Park <qkrtpwns1999@naver.com>
2021-12-16 17:17:32 +0900

    feat : 프론트 페이지에 컴포넌트 임시 배치

--- src/components/pages/Front.jsx
+++ src/components/pages/Front.jsx
@@ -1,11 +1,13 @@
 import TranslationToggleButton from "../UI/atoms/TranslationToggleButton";
 import BasicButton from "../UI/atoms/BasicButton";
+import RemoraHeader from "../UI/atoms/RemoraHeader";
 import ExtractStepper from "../UI/atoms/ExtractStepper";
 
 function Front() {
     return (
         <div className="ExtreactPage" style={{ padding: "1rem 0" }}>
             <h2>프론트페이지</h2>
+            <RemoraHeader index={0} />
             <BasicButton text="123123123" />
             <br />
             <TranslationToggleButton />

4c497cfc9b3a9aabf53ac2e2a5eeebf59273ac07
Sejun Park <qkrtpwns1999@naver.com>
2021-12-18 20:27:28 +0900

    refactor : 프론트페이지에 있던거 테스트 페이지로 이동

--- src/components/pages/Test.jsx
+++ src/components/pages/Test.jsx
@@ -1,4 +1,14 @@
+import BasicButton from "components/UI/atoms/BasicButton";
+import ExtractStepper from "components/UI/atoms/ExtractStepper";
+import TranslationToggleButton from "components/UI/atoms/TranslationToggleButton";
+
 function Test() {
-    return <>test</>;
+    return (
+        <>
+            <BasicButton text="123123123" />
+            <TranslationToggleButton />
+            <ExtractStepper />
+        </>
+    );
 }
 export default Test;

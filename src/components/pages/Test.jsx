8bff6b0599cf60ab3fd14a9b51dbe8273afd852f
Sejun Park <qkrtpwns1999@naver.com>
2021-12-20 19:20:39 +0900

    feat : about 컨텐츠 기본 틀 잡기

--- src/components/pages/Test.jsx
+++ src/components/pages/Test.jsx
@@ -1,19 +1,9 @@
-// import BasicButton from "components/UI/atoms/BasicButton";
-// import ExtractStepper from "components/UI/atoms/ExtractStepper";
-// import TranslationToggleButton from "components/UI/atoms/TranslationToggleButton";
-// import FrontContent from "components/UI/organisms/FrontContent";
-import ExtractContent from "components/UI/organisms/ExtractContent";
-// import FrontContent from "components/UI/organisms/FrontContent";
+import AboutContent from "../UI/organisms/AboutContent";
 
 function Test() {
     return (
         <>
-            {/* <FrontContent /> 
-            <BasicButton text="123123123" />
-            <TranslationToggleButton />
-            <ExtractStepper /> */}
-            {/* <FrontContent /> */}
-            <ExtractContent />
+            <AboutContent />
         </>
     );
 }

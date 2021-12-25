3903bd301fa42d074d7eae6ae3c4fc27aad6a031
Sejun Park <qkrtpwns1999@naver.com>
2021-12-25 17:54:56 +0900

    refactor : Theme 리팩터링, test 페이지 비우기

--- src/components/UI/atoms/BasicButton.jsx
+++ src/components/UI/atoms/BasicButton.jsx
@@ -1,16 +1,12 @@
 /* eslint-disable react/jsx-props-no-spreading */
 import { Button } from "@mui/material";
 import PropTypes from "prop-types";
-import { ThemeProvider } from "@mui/material/styles";
-import BasicTheme from "./BasicTheme";
 
 function BasicButton({ text, ...buttonProps }) {
     return (
-        <ThemeProvider theme={BasicTheme}>
-            <Button color="secondary" variant="contained" {...buttonProps}>
-                {text}
-            </Button>
-        </ThemeProvider>
+        <Button color="secondary" variant="contained" {...buttonProps}>
+            {text}
+        </Button>
     );
 }
 BasicButton.propTypes = {
--- src/components/pages/Root.jsx
+++ src/components/pages/Root.jsx
@@ -1,18 +1,22 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Front, About, Extract, Result, Test } from "components/pages";
+import { ThemeProvider } from "@mui/material";
+import theme from "components/UI/atoms/BasicTheme";
 
 function Root() {
     return (
-        <BrowserRouter>
-            <Routes>
-                <Route exact path="/" element={<Front />} />
-                <Route path="/about" element={<About />} />
-                <Route path="/extract" element={<Extract />} />
-                <Route path="/result" element={<Result />} />
-                <Route path="/test" element={<Test />} />
-                {/* [테스트] 반드시 지워주기 */}
-            </Routes>
-        </BrowserRouter>
+        <ThemeProvider theme={theme}>
+            <BrowserRouter>
+                <Routes>
+                    <Route exact path="/" element={<Front />} />
+                    <Route path="/about" element={<About />} />
+                    <Route path="/extract" element={<Extract />} />
+                    <Route path="/result" element={<Result />} />
+                    <Route path="/test" element={<Test />} />
+                    {/* [테스트] 반드시 지워주기 */}
+                </Routes>
+            </BrowserRouter>
+        </ThemeProvider>
     );
 }
 export default Root;

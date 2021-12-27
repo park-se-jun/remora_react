53927a468b92dd21bb89390cc774ed8974bec6f4
박세준 <qkrtpwns1999@naver.com>
2021-12-28 04:13:25 +0900

    URL 변경

--- src/modules/DefaultClient.js
+++ src/modules/DefaultClient.js
@@ -4,7 +4,7 @@ const baseURL = (() => {
     if (process.env.NODE_ENV === "development") {
         return "http://cors-anywhere.herokuapp.com/http://20.124.204.171:8080";
     }
-    return "http://remora-proxy.herokuapp.com/http://20.124.204.171:8080";
+    return "http://cors-anywhere.herokuapp.com/http://20.124.204.171:8080";
 })();
 
 const defaultClient = axios.create({

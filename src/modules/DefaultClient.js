885f3bac02bb82710351bff19424e49168f0bbdb
Sejun Park <qkrtpwns1999@naver.com>
2021-12-28 03:57:01 +0900

    프록시 서버 추가

--- src/modules/DefaultClient.js
+++ src/modules/DefaultClient.js
@@ -2,9 +2,9 @@ import axios from "axios";
 
 const baseURL = (() => {
     if (process.env.NODE_ENV === "development") {
-        return "http://20.124.204.171:8080";
+        return "http://cors-anywhere.herokuapp.com/http://20.124.204.171:8080";
     }
-    return "http://20.124.204.171:8080";
+    return "http://remora-proxy.herokuapp.com/http://20.124.204.171:8080";
 })();
 
 const defaultClient = axios.create({

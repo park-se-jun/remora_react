6a01dc17131b6f628e489a82ca3b7ae2c04354f2
Sejun Park <qkrtpwns1999@naver.com>
2021-12-28 12:36:17 +0900

    fix : conflict 해결

--- src/modules/DefaultClient.js
+++ src/modules/DefaultClient.js
@@ -4,11 +4,7 @@ const baseURL = (() => {
     if (process.env.NODE_ENV === "development") {
         return "http://20.124.204.171:8080";
     }
-<<<<<<< HEAD
     return "http://20.124.204.171:8080";
-=======
-    return "http://cors-anywhere.herokuapp.com/http://20.124.204.171:8080";
->>>>>>> develop
 })();
 
 const defaultClient = axios.create({
